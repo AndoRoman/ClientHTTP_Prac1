03cd4f05738591348c9c83b8f2e588ab269a844b
Armando R <20160415@ce.pucmm.edu.do>
2020-06-04 19:55:39 -0400

    Function to count Images into paragraph & Function to count Lines

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,6 +6,9 @@ import java.net.URL;
 
 public class main {
 
+    private static String[] filter = null;//A string that each position is a Paragraph.
+
+
     //Function to Validate the URL
     public static boolean URLisCorrect(String in){
         try{
@@ -22,25 +25,28 @@ public class main {
         return in.nextLine();
     }
     //Function to Count lines on Document
-    public static int LineCount(Document Input) {
-        try {
-            return Input.html().split(System.getProperty("Line.separator")).length;
-        }catch (Exception e){
-            return 0;
-        }
+    public static int LineCount(Document Input) { ;
+        return (Input.html().split("\n")).length;
     }
     //Function to Count <p> on Document
     public static int paraCount(Document Input) {
         int num = 0;
-        String[] filter = Input.html().split("<p>");
+        filter = Input.html().split("<p>");
         num = (filter.length)/2;
         return num;
     }
     //Function to Count <img> on Document
-    public static int imgCount(Document Input) {
-        int num = 0;
-
-        return num;
+    public static int imgCount() {
+        String[] filter2 = null;
+        if(filter.length > 1) {
+            for (int i = 0; i < filter.length; i++) {
+                filter2 = filter[i].split("img");
+            }
+            return filter2.length;
+        }
+        else {
+            return 0;
+        }
     }
 
     public static void main (String[] args) throws IOException {
@@ -61,11 +67,11 @@ public class main {
         //GET Document HTML to URL
         Document docHTML = Jsoup.connect(url).get();
         //Print Num of Line
-       // System.out.println("El Documento tiene: "+LineCount(docHTML)+" Lineas de codigo.");
+        System.out.println("El Documento tiene: "+LineCount(docHTML)+" Lineas de codigo.");
         //Print Num of <p>
         System.out.println("El Documento tiene: " + paraCount(docHTML)+ " Parrafos <p>");
         //Print Num of <img>
-        System.out.println("El Documento tiene: "+ " Imagenes");
+        System.out.println("El Documento tiene: " + imgCount()+" Imagenes dentro de los Parrafos");
     }
 
 
