04c16c2a85a007fbfdd7bb4f20598a14aa642ace
Armando R <20160415@ce.pucmm.edu.do>
2020-06-04 17:49:23 -0400

    GET document HTML from URL & Function to count Paragraph

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,6 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
 import java.util.Scanner;
 import java.net.URL;
 
@@ -18,9 +21,30 @@ public class main {
         Scanner in = new Scanner(System.in);
         return in.nextLine();
     }
+    //Function to Count lines on Document
+    public static int LineCount(Document Input) {
+        try {
+            return Input.html().split(System.getProperty("Line.separator")).length;
+        }catch (Exception e){
+            return 0;
+        }
+    }
+    //Function to Count <p> on Document
+    public static int paraCount(Document Input) {
+        int num = 0;
+        String[] filter = Input.html().split("<p>");
+        num = (filter.length)/2;
+        return num;
+    }
+    //Function to Count <img> on Document
+    public static int imgCount(Document Input) {
+        int num = 0;
 
-    public static void main (String[] args) {
-        String url = ReadURL();
+        return num;
+    }
+
+    public static void main (String[] args) throws IOException {
+       String url = ReadURL();
         boolean key = false;
         do{
             if(URLisCorrect(url))
@@ -33,8 +57,15 @@ public class main {
                 url = ReadURL();
             }
         } while (!key);
-
-
+        //String url = "https://www.example.org";
+        //GET Document HTML to URL
+        Document docHTML = Jsoup.connect(url).get();
+        //Print Num of Line
+       // System.out.println("El Documento tiene: "+LineCount(docHTML)+" Lineas de codigo.");
+        //Print Num of <p>
+        System.out.println("El Documento tiene: " + paraCount(docHTML)+ " Parrafos <p>");
+        //Print Num of <img>
+        System.out.println("El Documento tiene: "+ " Imagenes");
     }
 
 
