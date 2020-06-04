eb225a372f1de8ae1dd7c077cb1dee6530f4c5ec
Armando R <20160415@ce.pucmm.edu.do>
2020-06-04 12:52:48 -0400

    Function to Validate URL & refactor code structure

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,11 +1,42 @@
 import java.util.Scanner;
+import java.net.URL;
+
 public class main {
-    public static void main(String[] args) {
+
+    //Function to Validate the URL
+    public static boolean URLisCorrect(String in){
+        try{
+            new URL(in).toURI();
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+    //Function to Read console
+    public static String ReadURL(){
         System.out.println("Introduzca una URL: ");
         Scanner in = new Scanner(System.in);
-        String url = in.nextLine();
-        System.out.println("Haz introducido: "+url);
+        return in.nextLine();
+    }
+
+    public static void main (String[] args) {
+        String url = ReadURL();
+        boolean key = false;
+        do{
+            if(URLisCorrect(url))
+            {
+                System.out.println("Haz introducido: "+url);
+                key = true;
+            }
+            else {
+                System.out.println("La siguiente URL: "+url+ " No es valida.");
+                url = ReadURL();
+            }
+        } while (!key);
+
 
     }
+
+
 }
 
