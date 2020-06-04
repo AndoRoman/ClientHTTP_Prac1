605dfbca8e035dc95302aeed5841171e9c40da92
Armando R <20160415@ce.pucmm.edu.do>
2020-06-04 10:55:35 -0400

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Introduzca una URL: ");
+        Scanner in = new Scanner(System.in);
+        String url = in.nextLine();
+        System.out.println("Haz introducido: "+url);
+
+    }
+}
+
